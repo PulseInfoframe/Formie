a77c0061fb6f004d730a37bc67887d1c7b266cd6
Spencer Deinum <spencerdeinum@gmail.com>
2012-03-09 16:50:30 -0500

    Added a callback event for 'clearing' an element that the caller can override.

--- formie.js
+++ formie.js
@@ -3,14 +3,18 @@
 		// Default settings
 		var settings = $.extend({
 			// Default action: disable the specified element.
-			'action' : function( element, truthyness ) {
+			'action_event' : function( element, truthyness ) {
 				if( truthyness ) {
 					element.removeAttr('disabled');
 				}
 				else {
 					element.attr('disabled', 'disabled');
 				}
-			}
+			},
+			'clear_event': function( element ) {
+				element.val('');
+			},
+			'clear-values': true
 		}, options);
 
 		// Plugin code goes here to respect chaining.
@@ -19,21 +23,21 @@
 				var parentElement;
 				if( $(element).data('formieBind') ) {
 					parentElement = $('[name="' + $(element).data('formieBind') + '"]' ).last();
-					parentElement.bind('change.formie', createBoundHandler( $(element), parentElement, settings.action ) ).change();
+					parentElement.bind('change.formie', createBoundHandler( $(element), parentElement, settings.action_event, settings.clear_event ) ).change();
 				}
 				else if( $(element).data('formieIsChecked') ) {
 					parentElement = $('[name="' + $(element).data('formieIsChecked') + '"]' ).last();
-					parentElement.bind('change.formie', createCheckedHandler( $(element), parentElement, settings.action ) ).change();
+					parentElement.bind('change.formie', createCheckedHandler( $(element), parentElement, settings.action_event, settings.clear_event ) ).change();
 				}
 				else if( $(element).data('formieValueIn') ) {
 					parentElement = $('[name="' + $(element).data('formieValueIn') + '"]' ).last();
-					parentElement.bind('change.formie', createValueInHandler( $(element), parentElement, settings.action ) ).change();
+					parentElement.bind('change.formie', createValueInHandler( $(element), parentElement, settings.action_event, settings.clear_event ) ).change();
 				}
 			});
 		});
 	};
 
-	createBoundHandler = function( childElement, parentElement, action ) {
+	createBoundHandler = function( childElement, parentElement, action, clear ) {
 		var comparator = '';
 		var value = '';
 		var data = childElement.data();
@@ -63,28 +67,48 @@
 			}
 		}
 
+		var expression;
 		switch( comparator ) {
 			case '=':
-				return function() { action( childElement, parentElement.val() == value ); };
+				expression = function() { return parentElement.val() == value; };
+				break;
 			case '!=':
-				return function() { action( childElement, parentElement.val() != value ); };
+				expression = function() { return parentElement.val() != value; };
+				break;
 			case '>':
-				return function() { action( childElement, parentElement.val() > value ); };
+				expression = function() { return parentElement.val() > value; };
+				break;
 			case '<':
-				return function() { action( childElement, parentElement.val() < value ); };
+				expression = function() { return parentElement.val() < value; };
+				break;
 			case 'value_in':
-				return createValueInAction( childElement, parentElement, action );
+				return createValueInAction( childElement, parentElement, action, clear );
 		}
+
+		return createCallbacks( childElement, expression, action, clear );
 	};
 	
-	createCheckedHandler = function( childElement, parentElement, action ) {
-		return function() { action( childElement, parentElement.attr('checked') == 'checked' ); };
+	createCheckedHandler = function( childElement, parentElement, action, clear ) {
+		expression = function() {
+			return parentElement.attr('checked') == 'checked';
+		};
+		return createCallbacks( childElement, expression, action, clear );
 	};
 
-	createValueInAction = function( childElement, parentElement, action ) {
+	createValueInAction = function( childElement, parentElement, action, clear ) {
 		var valueInArray = childElement.data('formieValueIn');
 		var splitString = valueInArray.split(',');
-		return function() { action( childElement, $.inArray( parentElement.val(), splitString ) > -1 ); };
+		expression = function() {
+			return $.inArray( parentElement.val(), splitString ) > 1;
+		};
+		return createCallbacks( childElement, expression, action, clear );
+	};
+
+	createCallbacks = function( childElement, expression, action, clear ) {
+		return function() {
+			action( childElement, expression() );
+			clear( childElement );
+		};
 	};
 
 })( jQuery );
